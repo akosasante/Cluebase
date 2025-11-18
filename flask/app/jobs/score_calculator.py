15b8528113888b9de6bcb664ac66cdd4b1ac5a86
Akosua Asante <akosuaasante@gmail.com>
2025-11-17 20:50:34 -0500

    update scoring algorithm to avoid double counting timeout incorrect answers

--- flask/app/jobs/score_calculator.py
+++ flask/app/jobs/score_calculator.py
@@ -25,8 +25,13 @@ def calculate_scores(played_game_id):
         round2_correct = filter_clues('DJ!', lambda clue: clue.answer_state == AnswerState.CORRECT)
         
         # INCORRECT for Coryat: only INCORRECT and TIMEOUT_AFTER_BUZZ (not TIMEOUT_BEFORE_BUZZ)
-        round1_incorrect = filter_clues('J!', lambda clue: clue.answer_state in [AnswerState.INCORRECT, AnswerState.TIMEOUT_AFTER_BUZZ])
-        round2_incorrect = filter_clues('DJ!', lambda clue: clue.answer_state in [AnswerState.INCORRECT, AnswerState.TIMEOUT_AFTER_BUZZ])
+        # But for counting stats, only count INCORRECT (timeouts are separate)
+        round1_incorrect = filter_clues('J!', lambda clue: clue.answer_state == AnswerState.INCORRECT)
+        round2_incorrect = filter_clues('DJ!', lambda clue: clue.answer_state == AnswerState.INCORRECT)
+        
+        # For Coryat calculation, we need both INCORRECT and TIMEOUT_AFTER_BUZZ
+        round1_incorrect_for_coryat = filter_clues('J!', lambda clue: clue.answer_state in [AnswerState.INCORRECT, AnswerState.TIMEOUT_AFTER_BUZZ])
+        round2_incorrect_for_coryat = filter_clues('DJ!', lambda clue: clue.answer_state in [AnswerState.INCORRECT, AnswerState.TIMEOUT_AFTER_BUZZ])
         
         # SKIPPED: TIMEOUT_BEFORE_BUZZ and SKIPPED (do not affect Coryat score)
         round1_skipped = filter_clues('J!', lambda clue: clue.answer_state in [AnswerState.TIMEOUT_BEFORE_BUZZ, AnswerState.SKIPPED])
@@ -35,8 +40,8 @@ def calculate_scores(played_game_id):
         def calculate_coryat_score(correct_clues, incorrect_clues):
             return sum(clue.clue.value for clue in correct_clues) - sum(clue.clue.value for clue in incorrect_clues if not clue.clue.daily_double)
 
-        game.coryat_score_round1 = calculate_coryat_score(round1_correct, round1_incorrect)
-        game.coryat_score_round2 = calculate_coryat_score(round2_correct, round2_incorrect)
+        game.coryat_score_round1 = calculate_coryat_score(round1_correct, round1_incorrect_for_coryat)
+        game.coryat_score_round2 = calculate_coryat_score(round2_correct, round2_incorrect_for_coryat)
         game.coryat_score_total = game.coryat_score_round1 + game.coryat_score_round2
 
         game.round1_correct = len(round1_correct)
